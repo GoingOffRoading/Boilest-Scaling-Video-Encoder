436f06ed89fef48439cf8f62838420975a702c14
Chase Westlye <CWestlye@gmail.com>
2023-08-24 12:27:45 -0700

    asd

--- Encoding/snipets for later/ffprobe.py
+++ Encoding/snipets for later/ffprobe.py
@@ -1,1 +1,19 @@
 ffprobe -v quiet -print_format json -show_format -show_streams test.mkv
+
+
+
+
+import subprocess as sp
+import shlex
+import json
+
+video = 'test.mkv'
+
+# Execute ffprobe (to show streams), and get the output in JSON format
+data = sp.run(shlex.split(f'ffprobe -loglevel error -show_streams -of json {video}'), capture_output=True).stdout
+
+# Convert data from JSON string to dictionary
+d = json.loads(data)
+
+#print(d['codec_name'])
+print(json.dumps(d, indent = 4, sort_keys=True))
