1021a9bdd5237fa81f674ddd7ee3ad2886a1432c
Chase Westlye <CWestlye@gmail.com>
2023-08-25 13:41:17 -0700

    ffprobe progress

--- Encoding/snipets for later/subprocess.py
+++ Encoding/snipets for later/subprocess.py
@@ -1,6 +1,6 @@
 import subprocess
 
-result = subprocess.run(["ffmpeg -i test.mkv -c:v libsvtav1 -crf 20 -preset 5 -g 240 -pix_fmt yuv420p10le -c:s srt -c:a libopus testsrtlaopusasd.mkv"], shell=True, capture_output=True, text=True)
+result = subprocess.run(["ffmpeg -i test.mkv -c:v libsvtav1 -crf 20 -preset 4 -g 240 -pix_fmt yuv420p10le -c:s srt -c:a libopus testsrtlaopusasd.mkv"], shell=True, capture_output=True, text=True)
 
 print(result.stdout)
 
