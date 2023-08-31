de8758ef7d1e5bd0ad150ceb93b29b856f5a16fb
Chase Westlye <CWestlye@gmail.com>
2023-08-30 22:37:33 -0700

    progress

--- Worker/scripts/celery.sh
+++ Worker/scripts/celery.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/sh
 
 celery -A tasks celery beat -l debug &
@@ -9,11 +8,3 @@ celery -A tasks celery flower -l info
 #tail -f /dev/null
 
 
-=======
-#!/bin/sh -ex
-
-celery -A app.tasks.celery beat -l debug &
-celery -A app.tasks.celery worker -l info &
-celery -A app.tasks.celery flower -l info &
-tail -f /dev/null
->>>>>>> 674c8c6bb5ebf8ea61f7bbff40b5ab64508bf20e
