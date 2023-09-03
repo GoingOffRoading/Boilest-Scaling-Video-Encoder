bdba2c6519cc3c0407257d7059abb4e0d841ad41
Chase Westlye <CWestlye@gmail.com>
2023-09-03 11:36:24 -0700

    groan

--- Worker/scripts/tasks.py
+++ Worker/scripts/tasks.py
@@ -25,10 +25,10 @@ def fffinder(DIRECTORY):
 def ffprober(FILEPATH):
     # Execute ffprobe and get the codec of the first FILEPATH, audio, and subtitle stream
    
-    container = subprocess.run(shlex.split(f'ffprobe -v quiet -show_entries format=format_name -of default=noprint_wrappers=1:nokey=1 {FILEPATH}'), capture_output=True).stdout.decode('utf8').strip()       
-    vcodec = subprocess.run(shlex.split(f'ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 {FILEPATH}'), capture_output=True).stdout.decode('utf8').strip()
-    acodec = subprocess.run(shlex.split(f'ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 {FILEPATH}'), capture_output=True).stdout.decode('utf8').strip()
-    scodec = subprocess.run(shlex.split(f'ffprobe -v error -select_streams s:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 {FILEPATH}'), capture_output=True).stdout.decode('utf8').strip()
+    container = subprocess.run(shlex.split(f'ffprobe -v quiet -show_entries format=format_name -of default=noprint_wrappers=1:nokey=1 "{FILEPATH}"'), capture_output=True).stdout.decode('utf8').strip()       
+    vcodec = subprocess.run(shlex.split(f'ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "{FILEPATH}"'), capture_output=True).stdout.decode('utf8').strip()
+    acodec = subprocess.run(shlex.split(f'ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "{FILEPATH}"'), capture_output=True).stdout.decode('utf8').strip()
+    scodec = subprocess.run(shlex.split(f'ffprobe -v error -select_streams s:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "{FILEPATH}"'), capture_output=True).stdout.decode('utf8').strip()
 
     # Same as above, but outputs a larger object as json
     # I abandoned this approach as it was too dificult to loop through the JSON to find the first stream of each codec type (vido, audio, subtitle)
@@ -53,57 +53,84 @@ def ffprober(FILEPATH):
     if scodec != 'subrip':
         print (FILEPATH + ' is using ' + scodec + ', not SRT')
         encode = encode + ' -c:s srt'
+    
     # Now lets determine if we should pass a FILEPATH file to the next step, and what that step will cover
     if container == 'matroska,webm' and vcodec == 'av1' and acodec == 'opus' and scodec == 'subrip': 
         print (FILEPATH + ' is using all of the correct containers and codecs')
     else:
         job = {'file':FILEPATH, 'encode_string':encode}
         JOBJSON = json.dumps(job)
-        print(JOBJSON)
+        print('JSON is as follows: '+ JOBJSON)
         ffencode.delay(JOBJSON)
 
 @app.task
 def ffencode(JOBJSON):
     d = json.loads(JOBJSON)
 
-    input_file = (d["file"])
-    print (input_file)
+    input_file = '"' + (d["file"]) + '"'
+    print ('input file is: ' + input_file)
 
     ffmpeg_string = (d["encode_string"])
-    print (ffmpeg_string)
+    ffmpeg_string = ffmpeg_string + ' '
+    print ('ffmpeg string is: ' + ffmpeg_string)
 
     file_name = Path(input_file).stem
-    print (file_name)
+    file_name = '"/boil_hold/' + file_name + '.mkv"'
+    print ('file name is:' + file_name)
 
-    ffmpeg_string = 'ffmpeg -hide_banner -loglevel 16 -stats -i ' + input_file + ffmpeg_string + ' /boil_hold/'+ file_name + '.mkv'
+    ffmpeg_string = 'ffmpeg -hide_banner -loglevel 8 -stats -i ' + input_file + ffmpeg_string + file_name
     print (ffmpeg_string)
+    os.system(ffmpeg_string)
 
-    ffmpeg_string = ffmpeg_string.split()
-    print (ffmpeg_string)
-
-    process = subprocess.Popen(
-        ffmpeg_string,
-        stderr=subprocess.PIPE,
-        text=True,
-        bufsize=0)
-
-    print(process.stderr.read())
+    # I think I would like to do this as a subprocess instead of an OS call, but ran into too many issues having to split the string
+    # Specifically, ffmpeg_string... Splitting it created 'SyntaxError: invalid syntax'
+    # Given that it's going to take an investment of time to figure that out, and there's no advantage to the subprocess call of ffmpeg, I am going to leave it as an OS call above
+    #ffmpeg_string = ffmpeg_string.split()
+    #ffmpeg_string = 'ffmpeg, -hide_banner, -loglevel, 16, -stats, -i, ' + input_file + ', ', ffmpeg_string, ', /boil_hold/' + file_name + '.mkv'
+    # Inserting a list into a string
+    # https://stackoverflow.com/questions/56313176/how-to-fix-typeerror-can-only-concatenate-str-not-list-to-str
+    #print (ffmpeg_string)
+    # Current issues with the subprocess.popen is this error "TypeError: expected str, bytes or os.PathLike object, not list"
+    #process = subprocess.Popen(
+    #    ffmpeg_string,
+    #    stderr=subprocess.PIPE,
+    #    text=True,
+    #    bufsize=0)
+    #print(process.stderr.read())
 
     # Going to redo this later
     # Need to add data validations
+    # When we get to validations, we need to explore StatReport: https://gitlab.com/AOMediaCodec/SVT-AV1/-/blob/master/Docs/Parameters.md
 
-    if os.path.exists('/boil_hold/' + file_name + '.mkv') and os.path.exists(input_file):
+    #    >>>>>>>>>>>>>>>>>>>> HERE <<<<<<<<<<<<<<<<<<<<<<<<<
+    # Try this: https://stackoverflow.com/questions/3751900/create-file-path-from-variables
+
+
+
+
+    print ('now going to check on the existance of ' + file_name + ' and ' + input_file)
+
+    if os.path.exists(file_name):
+        print (file_name + ' exists')
+    else:
+        print (file_name + ' does not exist')   
+    if os.path.exists(input_file):
+        print (input_file + ' exists')
+    else:
+        print (input_file + ' does not exist')
+
+    if os.path.exists(file_name and input_file):
         print('Original and Encoded Files Exists')
         time.sleep(1)
         file_stats = os.stat(input_file)
         print(f'Original file Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}') 
-        file_stats2 = os.stat('/boil_hold/'+ file_name + '.mkv')
+        file_stats2 = os.stat(file_name)
         print(f'Encoded file Size in MegaBytes is {file_stats2.st_size / (1024 * 1024)}') 
-        print('Removing the original file from ' + file)
-        os.remove(file) 
+        print('Removing the original file from ' + input_file)
+        os.remove(input_file) 
         time.sleep(1)
         print('Moving the encoded file')
-        os.rename('./boil_hold/' + file, file)
+        os.rename(file_name, input_file)
         print ('Done')    
     else:
          print("Something Broke")
