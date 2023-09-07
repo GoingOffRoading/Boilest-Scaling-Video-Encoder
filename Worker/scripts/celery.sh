6590fc84b8cec695e9f7bd0aca0c36a469d8df25
Chase Westlye <CWestlye@gmail.com>
2023-09-07 15:47:46 -0700

    gogo

--- Worker/scripts/celery.sh
+++ Worker/scripts/celery.sh
@@ -1,10 +1,7 @@
+
 #!/bin/sh
 
 celery -A tasks celery beat -l debug &
 celery -A tasks celery worker -l info &
-celery -A tasks celery flower -l info 
-
-#&
-#tail -f /dev/null
-
-
+celery -A tasks celery flower -l info &
+tail -f /dev/null
