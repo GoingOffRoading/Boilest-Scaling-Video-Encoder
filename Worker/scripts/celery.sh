ff4971fa35f04aaa7ba5661c890763d4d81936f8
Chase Westlye <CWestlye@gmail.com>
2023-09-27 22:32:40 -0700

    asd

--- Worker/scripts/celery.sh
+++ Worker/scripts/celery.sh
@@ -4,17 +4,10 @@ echo "Checking Variables"
 echo $Manager
 echo $Worker
 
-if [ $Manager == "Yes" -a $Worker == "Yes" ]; then
+if [ $Manager == "Yes" ]; then
     echo "Running Manager and Worker" 
-    celery -A tasks worker -B -l info -c 1 -Q worker,manager &
-    celery -A tasks flower -l debug 
-elif [ $Mmanager == "Yes" -a $Worker == "No" ]; then
-    echo "Running Manager" 
     celery -A tasks worker -B -l info -c 1 -Q manager &
     celery -A tasks flower -l debug 
-elif [ $Manager == "No" -a $Worker == "Yes" ]; then
-    echo "Running Worker" 
-    celery -A tasks worker -B -l info -c 1 -Q worker 
 else
-    echo "We have a problem"
+    celery -A tasks worker -B -l info -c 1 -Q worker 
 fi
