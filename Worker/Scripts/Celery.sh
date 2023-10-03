f459ea9a70617c54d6d2c1584c13cb820bf41f6f
Chase Westlye <CWestlye@gmail.com>
2023-10-03 06:38:35 -0700

    asd

--- Worker/Scripts/Celery.sh
+++ Worker/Scripts/Celery.sh
@@ -5,14 +5,14 @@ echo $Manager
 
 if [ $Manager == "Yes" ]; then
     echo "Running Manager" 
-    celery -A tasks worker -B -l info -c 1 -Q manager -n manager@%n &
-    celery -A tasks flower -l debug 
+    celery -A tasks worker -B -l WARNING -c 1 -Q manager -n manager@%n &
+    celery -A tasks flower -l WARNING 
 
 elif [ $Manager == "Yes"] -a [ $Worker == "Yes"]; then
     echo "Running Worker & Manager" 
-    celery -A tasks worker -B -l info -c 1 -Q manager,worker -n manager@%n &
-    celery -A tasks flower -l debug 
+    celery -A tasks worker -B -l WARNING -c 1 -Q manager,worker -n manager@%n &
+    celery -A tasks flower -l WARNING 
 else
     echo "Running Worker" 
-    celery -A tasks worker -B -l info -c 1 -Q worker -n worker@%n
+    celery -A tasks worker -B -l WARNING info -c 1 -Q worker -n worker@%n
 fi
