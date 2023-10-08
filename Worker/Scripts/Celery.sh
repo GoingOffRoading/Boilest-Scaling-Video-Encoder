55ae92f15b15c66f54029aa3474ef1979dbbbb36
Chase Westlye <CWestlye@gmail.com>
2023-10-08 13:29:53 -0700

    Fun

--- Worker/Scripts/Celery.sh
+++ Worker/Scripts/Celery.sh
@@ -17,11 +17,11 @@ echo "Starting Celery"
 if [ $Manager = "Yes" -a $Worker = "No" ]; then
     echo "Running Manager" 
     celery -A tasks worker -B -l WARNING -c 1 -Q manager,prober -n manager@%n &
-    celery -A tasks flower -l WARNING 
+    celery -A tasks flower -l WARNING
 elif [ $Manager = "Yes" -a $Worker = "Yes" ]; then
     echo "Running Worker & Manager" 
     celery -A tasks worker -B -l WARNING -c 1 -Q manager,worker,prober -n manager@%n &
-    celery -A tasks flower -l WARNING 
+    celery -A tasks flower -l WARNING
 elif [ $Manager = "No" -a $Worker = "Yes" ]; then
     echo "Running Worker" 
     celery -A tasks worker -B -l WARNING -c 1 -Q worker,prober -n worker@%n
