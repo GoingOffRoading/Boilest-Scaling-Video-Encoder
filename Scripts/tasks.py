97ba293703e4fe4d2a40f29886618b69a90a61fb
Chase Westlye <CWestlye@gmail.com>
2023-11-04 01:04:16 -0700

    Updates

--- Scripts/tasks.py
+++ Scripts/tasks.py
@@ -137,14 +137,14 @@ def fprober(ffinder_json):
         if d['streams'][i]['codec_type'] == 'video':
             codec_name = d['streams'][i]['codec_name'] 
             original_string = original_string + '{' + str(i) + d['streams'][i]['codec_type'] + '=' + codec_name + '}'
-            if codec_name == 'av1':
+            if codec_name == (ffinder_json["ffmpeg_video_codec"]):
                 encode_string = encode_string + ' -map 0:' + str(i) + ' -c:v copy'
                 # No need to change encode_decision as the video codec is in the desired format
                 print ('Stream ' + str(i) + ' is already ' + codec_name + ', copying stream')
             elif codec_name == 'mjpeg':
                 print ('Garbage mjpeg stream, ignoring')    
                 # No use for this for now
-            elif codec_name != 'av1':
+            elif codec_name != (ffinder_json["ffmpeg_video_codec"]):
                 encode_decision = 'yes'
                 encode_string = encode_string + ' -map 0:' + str(i) + ' ' + (ffinder_json["ffmpeg_video_string"])
                 print ('Stream ' + str(i) + ' is ' + codec_name + ', encoding stream')
@@ -155,7 +155,7 @@ def fprober(ffinder_json):
         elif d['streams'][i]['codec_type'] == 'audio':
             codec_name = d['streams'][i]['codec_name'] 
             original_string = original_string + '{' + str(i) + d['streams'][i]['codec_type'] + '=' + codec_name + '}'
-            if codec_name == 'opus':
+            if codec_name == (ffinder_json["ffmpeg_audio_codec"]):
                 encode_string = encode_string + ' -map 0:' + str(i) + ' -c:a copy'
                 print ('Stream ' + str(i) + ' is already ' + codec_name + ': nothing to encode')
                 # No need to change encode_decision as the audio codec is in the desired format
@@ -164,7 +164,7 @@ def fprober(ffinder_json):
                 encode_decision = 'yes'
                 print ('Stream ' + str(i) + ' is ' + codec_name + ' and, using ' + d['streams'][i]['channel_layout'] +', filtering, then encoding stream')
                 # encode_decision = yes as the audio codec is not in the desired format... libopus has problems with 5.1(side) channel layout in ffmpeg so we catch it here.  Unclear if we can combine this step with the next one
-            elif codec_name != 'opus':
+            elif codec_name != (ffinder_json["ffmpeg_audio_codec"]):
                 #encode_string = encode_string + ' -map 0:' + str(i) + ' -acodec libopus' 
                 encode_string = encode_string + ' -map 0:' + str(i) + ' ' + (ffinder_json["ffmpeg_audio_string"])
                 encode_decision = 'yes'
@@ -176,16 +176,16 @@ def fprober(ffinder_json):
         elif d['streams'][i]['codec_type'] == 'subtitle':
             codec_name = d['streams'][i]['codec_name'] 
             original_string = original_string + '{' + str(i) + d['streams'][i]['codec_type'] + '=' + codec_name + '}'
-            if codec_name == 'subrip':
+            if codec_name == (ffinder_json["ffmpeg_subtitle_format"]):
                 encode_string = encode_string + ' -map 0:' + str(i) + ' -c:s copy'
                 # No need to change encode_decision as the subtitles are in the desired format
                 print ('Stream ' + str(i) + ' is already ' + codec_name + ': nothing to encode')
-            elif codec_name != 'subrip' and d['streams'][i]['codec_name'] == 'hdmv_pgs_subtitle':
+            elif d['streams'][i]['codec_name'] == 'hdmv_pgs_subtitle':
                 # These are image based subtitles, and can't be converted to text type
                 encode_string = encode_string + ' -map 0:' + str(i) + ' -c:s copy' 
                 # No need to change encode_decision as we can't change picture subtitles into text subtitles
                 print ('Stream ' + str(i) + ' is ' + codec_name + ': a pain in the dick, and nothing we can do but copy the stream')
-            elif codec_name != 'subrip':
+            elif codec_name != (ffinder_json["ffmpeg_subtitle_format"]):
                 #encode_string = encode_string + ' -map 0:' + str(i) + ' -scodec subrip' 
                 encode_string = encode_string + ' -map 0:' + str(i) + ' ' + (ffinder_json["ffmpeg_subtitle_string"]) 
                 encode_decision = 'yes'
