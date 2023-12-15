fe5f3c66f52c722f2e760c9377236f4b4df8dd0f
Chase Westlye <CWestlye@gmail.com>
2023-12-14 21:38:56 -0800

    progress

--- Scripts/task_shared_services.py
+++ Scripts/task_shared_services.py
@@ -107,4 +107,27 @@ def ffmpeg_output_file(file_path):
     return ffmpeg_temp_output
 
 
+def run_ffmpeg(input_file, output_file, ffmpeg_args):
+    cmd = ['ffmpeg', '-i', input_file] + ffmpeg_args + [output_file]
+
+    try:
+        # Start the FFmpeg process with stdout and stderr as pipes
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        # Stream stdout and stderr in real-time
+        for line in process.stdout:
+            print(f"STDOUT: {line.strip()}")
+
+        for line in process.stderr:
+            print(f"STDERR: {line.strip()}")
+
+        # Wait for the process to complete
+        exit_code = process.wait()
+
+        print("FFmpeg encoding completed")
+        return exit_code
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return 1  # Return a non-zero exit code to indicate an error
 
