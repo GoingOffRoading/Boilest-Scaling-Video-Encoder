43e06f590b03d388276ece13bc9e9b492f1ca2a0
Chase Westlye <CWestlye@gmail.com>
2024-02-11 15:41:21 -0800

    No need to run flask on workers

--- Scripts/Celery.sh
+++ Scripts/Celery.sh
@@ -3,12 +3,6 @@ echo "starting"
 echo "Getting this party started"
 python /Scripts/container_start.py
 
-echo "Starting Flask"
-
-#flask run
-nohup flask run > log.txt 2>&1 &
-
-echo "Flask Started"
 
 echo "Starting Celery"
 echo "Checking Variables"
@@ -16,6 +10,9 @@ echo "Manager is set to:" $Manager
 
 
 if [ $Manager = "Yes" ]; then
+    echo "Starting Flask"
+    nohup flask run > log.txt 2>&1 &
+    echo "Flask Started"
     echo "Running Manager" 
     celery -A tasks_manager worker -B -l INFO -Q manager -n manager@%n &
     celery --broker=amqp://celery:celery@192.168.1.110:31672/celery flower
