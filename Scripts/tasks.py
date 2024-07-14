a6fb67b70884a06f81eb9f02e0855d113e761a83
Chase Westlye <CWestlye@gmail.com>
2024-07-14 10:37:11 -0700

    11

--- Scripts/tasks.py
+++ Scripts/tasks.py
@@ -8,6 +8,54 @@ app = Celery('worker', backend = celery_url_path('rpc://'), broker = celery_url_
 app.config_from_object(celeryconfig)
 
 
+@app.task(queue='worker', name='queue_workers_if_queue_empty')
+def queue_workers_if_queue_empty(arg):
+    try:
+        queue_depth = check_queue('worker')
+        
+        logging.info(f'Current Worker queue depth is: {queue_depth}')
+        print(f'Current Worker queue depth is: {queue_depth}')
+        
+        if queue_depth == 0:
+            logging.debug('Starting locate_files')
+            # >>>>>>>>>>><<<<<<<<<<<<<<<<
+            # >>>>>>>>>>><<<<<<<<<<<<<<<<
+            locate_files.apply_async(kwargs={'arg': arg}, priority=1)
+            # >>>>>>>>>>><<<<<<<<<<<<<<<<
+            # >>>>>>>>>>><<<<<<<<<<<<<<<<
+        elif queue_depth > 0:
+            logging.debug(f'{queue_depth} tasks in queue. No rescan needed at this time.')
+        else:
+            logging.error('Something went wrong checking the Worker Queue')
+    
+    except Exception as e:
+        logging.error(f"Error in queue_workers_if_queue_empty: {e}")
+
+
+def check_queue(queue_name):
+    try:
+        rabbitmq_host = 'http://' + os.environ.get('rabbitmq_host','192.168.1.110')
+        rabbitmq_port = os.environ.get('rabbitmq_port','32311')
+        user = os.environ.get('user','celery')
+        password = os.environ.get('password','celery')
+        celery_vhost = os.environ.get('celery_vhost','celery')
+
+        url = f"{rabbitmq_host}:{rabbitmq_port}/api/queues/{celery_vhost}/{queue_name}"
+        logging.debug(f'Checking RabbitMQ queue depth for: {queue_name}')
+
+        response = requests.get(url, auth=(user, password))
+        response.raise_for_status()  # Ensure we raise an exception for HTTP errors
+
+        worker_queue = response.json()
+        queue_depth = worker_queue.get("messages_unacknowledged", 0)
+
+        logging.debug (f'check_queue queue depth is: ' + str(queue_depth))
+        return queue_depth
+    except Exception as e:
+        logging.error(f"Error getting active tasks count: {e}")
+        return -1 # Return -1 to indicate an error
+
+
 # >>>>>>>>>>>>>>>>>>>>>> <<<<<<<<<<<<<<<<<<<<<< 
 # This section starts the discovery of the files by searching directories
 # >>>>>>>>>>>>>>>>>>>>>> <<<<<<<<<<<<<<<<<<<<<<
