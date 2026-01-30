c48ba4831bcc4e3aae1f3fdf31d8b20eab890036
Chase Westlye <CWestlye@gmail.com>
2026-01-29 20:48:49 -0800

    Boil.db tweak

--- Scripts/Manager/scripts/db_path.py
+++ Scripts/Manager/scripts/db_path.py
@@ -2,7 +2,7 @@ import os
 
 def get_db_path():
     if os.environ.get("role") == "manger":
-        db_path = "/boil/manager/scripts/boilest.db"
+        db_path = "/Boil/App/boilest.db"
     else:
         db_path = 'boilest.db'
     return db_path
