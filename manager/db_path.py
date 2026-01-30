238d038e96ed376f94ce42189d3432b9a5d6dcb8
Chase Westlye <CWestlye@gmail.com>
2026-01-29 22:23:46 -0800

    Update db_path.py

--- manager/db_path.py
+++ manager/db_path.py
@@ -1,8 +1,8 @@
 import os
 
 def get_db_path():
-    if os.environ.get("Role") == "Manger":
-        db_path = "/Boil/App/boilest.db"
+    if os.environ.get("Role") == "Manager":
+        db_path = "/boil/app/boilest.db"
     else:
         db_path = 'boilest.db'
     return db_path
